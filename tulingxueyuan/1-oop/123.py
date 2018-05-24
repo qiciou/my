7c4095d62f55b1c9e43de6853b260f2db806da4e
qiciou <1414299519@qq.com>
2018-05-24 14:57:34 +0800

    by lee

---
+++ tulingxueyuan/1-oop/123.py
@@ -0,0 +1,11 @@
+class Student():
+    name = None
+    age = 15
+    course = "python"
+    def dohomework(self):
+        print("homework doing")
+        return None
+a = Student()
+print(a.name)
+print(a.age)
+a.dohomework()
